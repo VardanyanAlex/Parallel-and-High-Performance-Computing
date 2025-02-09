4bfe6cfb09cf3d2fb1827b1cf5a186fb02875461
VardanyanAlex <vardanyan2001alex@gmail.com>
2025-02-09 12:08:48 +0400

    assignment5 correction

--- workshop/hw/hw1_pointers/assignment5.c
+++ workshop/hw/hw1_pointers/assignment5.c
@@ -5,7 +5,7 @@
 int main()
 {
 	int* p_num = (int*)malloc(sizeof(int));
-	if(p_num == NULL)
+	if (p_num == NULL)
 	{
 		printf("Couldn't allocate memory!\n");
 		return -1;
@@ -15,6 +15,12 @@ int main()
 	printf("%d\n", *p_num);
 
 	int* p_arr = (int*)malloc(sizeof(int) * 5);
+	if (p_arr == NULL)
+	{
+		printf("Couldn't allocate memory for the array\n");
+		return -1;
+	}
+	
 	for (int i = 0; i < 5; ++i)
 	{
 		*(p_arr + i) = i;
